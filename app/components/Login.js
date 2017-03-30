e20227a16542d2e2dfcd28b7e2f2fdf1213cec84
Charles Dunn <dunnc1551@gmail.com>
2017-03-30 13:56:14 -0600

    redirect to homepage on successful login

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
 
 export default class Login extends Component {
   constructor() {
@@ -12,7 +14,7 @@ export default class Login extends Component {
 
   login(e){
     e.preventDefault();
-    const { signIn, signInFailed } = this.props;
+    const { signIn, signInFailed, history } = this.props;
     const { email, password } = this.state;
 
     fetch('http://localhost:3000/api/users', {
@@ -26,7 +28,7 @@ export default class Login extends Component {
       }
       else {
         response.json().then(user => signIn(user.data))
-        // redirect??
+        history.push('/')
       }
     })
     .catch(error => {
@@ -36,27 +38,31 @@ export default class Login extends Component {
 
   render() {
     return(
-      <form>
-        { this.state.error && <p className="errorMessage">{ this.state.error }</p>}
-        <input
-          type="email"
-          name="email"
-          value={this.state.email}
-          onChange={(e) => this.setState({ email: e.target.value })}
-          placeholder="Email"
-          required
-          />
-        <input
-          type="password"
-          name="email"
-          value={this.state.password}
-          minLength="4"
-          onChange={(e) => this.setState({ password: e.target.value })}
-          placeholder="Password"
-          required
-          />
-        <button onClick={ (e) => this.login(e) }>Submit</button>
-      </form>
+      <div>
+        <form>
+          { this.state.error && <p className="errorMessage">{ this.state.error }</p>}
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={(e) => this.setState({ email: e.target.value })}
+            placeholder="Email"
+            required
+            />
+          <input
+            type="password"
+            name="email"
+            value={this.state.password}
+            minLength="4"
+            onChange={(e) => this.setState({ password: e.target.value })}
+            placeholder="Password"
+            required
+            />
+          <button onClick={ (e) => this.login(e) }>Submit</button>
+        </form>
+        <Link to="/new-user">Create an account</Link>
+      </div>
+
     )
   }
 }
