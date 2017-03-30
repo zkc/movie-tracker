e0c4d6d537248a0a3eb58ad1a4f5272a1565b229
Kyle Zucker <zkyle12@gmail.com>
2017-03-30 16:42:15 -0600

    add display of favorites. move favorite array into store out of state of favorite compo.

--- app/containers/FavoriteContainer.js
+++ app/containers/FavoriteContainer.js
@@ -1,9 +1,19 @@
 import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 import Favorites from '../components/Favorites.js';
+import * as actions from '../actions/actions.js';
+
 
 const mapStateToProps = (state)=>  {
   return state
 };
 
-export default connect(mapStateToProps, null)(Favorites);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addMovies: (movies) => {
+      dispatch(actions.addMovies(movies))
+    }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
