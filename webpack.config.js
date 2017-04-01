046eb2fc7fa08fa280b454a7bce4f4369f08b56b
Charles Dunn <dunnc1551@gmail.com>
2017-04-01 12:25:14 -0600

    update loaders

--- webpack.config.js
+++ webpack.config.js
@@ -19,12 +19,11 @@ module.exports = {
        include: path.join(__dirname, 'app'),
        exclude: /node_modules/,
        query: {
-         presets: ['es2015', 'react', 'stage-0']
-       },
-       { test: /\.css$/, loader: "style!css" },
-       { test: /\.scss$/, loader: "style!css!sass" },
-       { test: /\.svg$/, loader: 'svg-inline-loader'}
-     }]
+         presets: ['es2015', 'react']
+       }
+     },
+     { test: /\.css$/, loader: "style!css" },
+     { test: /\.svg$/, loader: 'svg-url-loader'}]
    },
   resolve: {
     extensions: ['', '.js', '.jsx', '.json', '.scss', '.css']
