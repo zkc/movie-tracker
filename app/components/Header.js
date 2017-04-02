c17adc5a486e4745843ba680afebc14cbf9d88ca
Charles Dunn <dunnc1551@gmail.com>
2017-04-01 21:34:08 -0600

    update classNames to be more semantic

--- app/components/Header.js
+++ app/components/Header.js
@@ -10,17 +10,17 @@ class Header extends Component {
   }
 
   render() {
-  const { email, name } = this.props.user;
+  const { email, name, } = this.props.user;
   return (
     <header>
-      <div className="title">
+      <div className="sign-in-wrap">
         { email && <p className="welcome"><img className="profile" src="../assets/styles/images/user1.svg" />Welcome, { name }</p> }
         { !email && <NavLink className="sign-in" to="/login">Sign in<img className="arrow" src="../assets/styles/images/arrow-double.svg"/></NavLink> }
         { email && <NavLink className="sign-in" to="/" onClick={() => this.props.signOut()}>Sign out<img className="arrow" src="../assets/styles/images/arrow-double.svg"/></NavLink> }
       </div>
       <NavLink to="/"><h1>Movie Tracker</h1></NavLink>
 
-      <div className="user-info">
+      <div className="view-favorites">
         { email && <NavLink to="/favorites"><img className="star" src="../assets/styles/images/Star_icon.svg" />View Favorites</NavLink> }
       </div>
     </header>
