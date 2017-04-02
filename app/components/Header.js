b1794a7030b4e9002543083dc2d01138b2d4e2af
Charles Dunn <dunnc1551@gmail.com>
2017-04-01 21:44:33 -0600

    move favorites underneath welcome user

--- app/components/Header.js
+++ app/components/Header.js
@@ -14,15 +14,16 @@ class Header extends Component {
   return (
     <header>
       <div className="sign-in-wrap">
-        { email && <p className="welcome"><img className="profile" src="../assets/styles/images/user1.svg" />Welcome, { name }</p> }
+        <div className="view-favorites">
+          { email && <p className="welcome"><img className="profile" src="../assets/styles/images/user1.svg" />Welcome, { name }</p> }
+          { email && <NavLink to="/favorites"><img className="star" src="../assets/styles/images/Star_icon.svg" />View Favorites</NavLink> }
+        </div>
         { !email && <NavLink className="sign-in" to="/login">Sign in<img className="arrow" src="../assets/styles/images/arrow-double.svg"/></NavLink> }
         { email && <NavLink className="sign-in" to="/" onClick={() => this.props.signOut()}>Sign out<img className="arrow" src="../assets/styles/images/arrow-double.svg"/></NavLink> }
       </div>
       <NavLink to="/"><h1>Movie Tracker</h1></NavLink>
 
-      <div className="view-favorites">
-        { email && <NavLink to="/favorites"><img className="star" src="../assets/styles/images/Star_icon.svg" />View Favorites</NavLink> }
-      </div>
+
     </header>
     )
   }
