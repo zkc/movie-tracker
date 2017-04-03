6e3787565a0ab35ab842d71fe685d1fe37f41dd6
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-04-02 19:13:07 -0600

    add one passing test for MovieIndex. one not working for componentDidMount test. installed sinon for spy

--- app/tests/components/MovieIndex.test.js
+++ app/tests/components/MovieIndex.test.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import fetchMock from 'fetch-mock'
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import fetchMock from 'fetch-mock';
+import sinon from 'sinon';
 
-import App from '../../components/App.js';
+import MovieIndex from '../../components/MovieIndex.js';
 
-describe('App', () => {
+function setup() {
 
-  it('should return App Component', () => {
-    expect(true).toBe(true)
+  const props = {
+    movies:[],
+    history: {},
+    location: {
+      pathname: '/'
+      },
+    match: {}
+  };
+
+  const wrapper = shallow(<MovieIndex {...props} />)
+  const mountWrapper = mount(<MovieIndex {...props} />)
+
+  return {
+    props,
+    wrapper
+  }
+};
+
+describe('MovieIndex', () => {
+
+  it('should render MovieIndex components', () => {
+    const { wrapper } = setup()
+
+    expect(wrapper.find('.movie-container').length).toEqual(1);
   });
+
+
+  xit('calls componentDidMount', () => {
+    const { mountWrapper } = setup()
+    sinon.spy(MovieIndex.prototype, 'componentDidMount');
+    expect(MovieIndex.prototype.componentDidMount).toHaveProperty('callCount', 1);
+    App.prototype.componentDidMount.restore();
+});
 })
