66d15a74b0a1ee953539bf91ec85af918d0e139e
Charles Dunn <dunnc1551@gmail.com>
2017-04-03 08:40:21 -0600

    add tests for new user sign in

--- app/tests/components/NewUser.test.js
+++ app/tests/components/NewUser.test.js
@@ -6,18 +6,49 @@ import NewUser from '../../components/NewUser.js';
 
 describe('NewUser', () => {
 
-  it('should return NewUser Component', async (done) => {
+  const mockUser = {
+    data: {
+      name: 'Charlie',
+      id: 1,
+      email: 'test@example.com'
+    }
+  }
+
+  afterEach(() => {
+    expect(fetchMock.calls().unmatched).toEqual([]);
+    fetchMock.restore();
+  })
+
+  it('should display an error if no email address is added', async (done) => {
     fetchMock.post(`http://localhost:3000/api/users/new`, { status: 500, body: {} })
 
-    const wrapper = shallow(<NewUser />)
+    const wrapper = shallow(<NewUser user={mockUser.data} signIn={jest.fn()} />)
 
     const button = wrapper.find('button')
 
     button.simulate('click')
-
     await wrapper.update()
 
     expect(wrapper.state().error).toBe('Email has already been used')
-    done()
+
+    done();
   });
-})
+
+  it('should create a new account on successful login', async (done) => {
+
+    fetchMock.post(`http://localhost:3000/api/users/new`, {
+      status: 200,
+      ok: true,
+      body: mockUser.data });
+
+    const wrapper = shallow(<NewUser user={mockUser.data} signIn={jest.fn()} />)
+
+    const button = wrapper.find('.sign-in-btn');
+
+    button.simulate('click');
+    await wrapper.update();
+
+    expect(fetchMock.calls().matched.length).toEqual(1);
+    done();
+  });
+});
